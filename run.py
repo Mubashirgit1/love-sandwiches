4728218f5e136d86295ccaea2bdb11857a0741c3
Mubashirgit1 <mubashir.connect@gmail.com>
2025-08-23 18:38:22 +0100

    get sale data from user

--- run.py
+++ run.py
@@ -11,6 +11,22 @@ CREDS  = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
-sales = SHEET.worksheet('sales')
-data = sales.get_all_values()
-print(data)
+
+# sales = SHEET.worksheet('sales')
+# data = sales.get_all_values()
+# print(data)
+
+def get_sales_data(): 
+    """
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is: {data_str}")
+
+get_sales_data()
