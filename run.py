df81b303cefcaa268a6b47d2298d9bb503c85367
Mubashirgit1 <mubashir.connect@gmail.com>
2025-09-15 19:31:17 +0100

    get last 5 values of sandwiches

--- run.py
+++ run.py
@@ -76,6 +76,19 @@ def calculate_surplus_data(sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
     return surplus_data
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    column = sales.col_values(3)
+    columns = []
+    for ind in range(1,7):
+      column = sales.col_values(ind)
+      columns.append(column[-5:])
+    return columns
 def main(): 
     """
     Run all program functions
@@ -88,4 +101,5 @@ def main():
     print(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+get_last_5_entries_sales()
